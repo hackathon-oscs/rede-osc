289408d4927082acdfcd118604e1ee42400a47a3
Carlos M. dos Santos, Sir <carlos-morais.santos@serpro.gov.br>
2014-05-29 05:51:31 +0400

    Na falta de um tema, trocamos o logotipo de boas-vindas.

--- app/models/environment.rb
+++ app/models/environment.rb
@@ -259,7 +259,7 @@ class Environment < ActiveRecord::Base
   settings_items :location, :type => String
   settings_items :layout_template, :type => String, :default => 'default'
   settings_items :homepage, :type => String
-  settings_items :description, :type => String, :default => '<div style="text-align: center"><a href="http://noosfero.org/"><img src="/images/noosfero-network.png" alt="Noosfero"/></a></div>'
+  settings_items :description, :type => String, :default => '<div style="text-align: center"><a href="http://noosfero.org/"><img src="/images/noosfero-network.png" alt="Participe em Rede!"/></a></div>'
   settings_items :local_docs, :type => Array, :default => []
   settings_items :news_amount_by_folder, :type => Integer, :default => 4
   settings_items :help_message_to_add_enterprise, :type => String, :default => ''
